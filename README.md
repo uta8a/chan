96aa6934e18d93e62261c748ae7a365b20dc8a90
uta8a <noumodulity@gmail.com>
2020-01-08 15:34:16 +0900

    add: gen template file

---
+++ README.md
@@ -0,0 +1,19 @@
+# chan
+競技プログラミング用CLIツール
+ディレクトリ名など、ローカルなものに依存するので僕以外の環境での動作は想定していません。
+
+# 使い方
+**compile**
+```
+chan main.cpp
+chan main.rs
+```
+**generate library template**
+```
+chan g rust hoge
+```
+- generate `uta8alib/src/rust/hoge/main.rs`
+```
+chan g cpp fuga
+```
+- generate `uta8alib/src/rust/fuga/main.cpp`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+use std::env;
+use std::io::{Write, BufWriter};
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut path = env::current_dir().unwrap();
+    path.pop();
+    if path.ends_with("uta8alib") == false {
+        println!("Error: cannot found directory uta8alib/");
+        panic!();
+    }
+    path.push("src");
+    let args:Vec<String> = env::args().collect();
+    if args.len() == 2 {
+        // compile
+        
+    }else if args.len() == 4 {
+        // gen lib
+        let gen = args[1].as_str();
+        let lang = args[2].as_str();
+        match gen {
+            "g" | "generate" => {
+                match lang {
+                    "r" | "rust" => {
+                        path.push("rust");
+                        path.push(args[3].as_str());
+                        match fs::create_dir(path.clone()) {
+                            Ok(_) => {},
+                            Err(e) => {
+                                println!("{}", e);
+                                println!("Error: Folder already Exists");
+                                panic!();
+                            }
+                        }
+                        path.push("main.rs");
+                        let template = include_str!("../lang-template/rust/main.rs");
+                        let mut f = BufWriter::new(fs::File::create(path).unwrap());
+                        f.write(template.as_bytes()).unwrap();
+                    },
+                    "c" | "cpp" => {
+                        path.push("cpp");
+                        path.push(args[3].as_str());
+                        match fs::create_dir(path.clone()) {
+                            Ok(_) => {},
+                            Err(e) => {
+                                println!("{}", e);
+                                println!("Error: Folder already Exists");
+                                panic!();
+                            }
+                        }
+                        path.push("main.cpp");
+                        let template = include_str!("../lang-template/cpp/main.cpp");
+                        let mut f = BufWriter::new(fs::File::create(path).unwrap());
+                        f.write(template.as_bytes()).unwrap();
+                    },
+                    _ => {
+                        println!("Error: select valid language, rust `r` or cpp `c`");
+                    }
+                }
+            },
+            _ => {
+                println!("Error: second arg is `g` or `generate`");
+            }
+        }
+    }else{
+        println!("Error: arg is 1 or 3 words");
+    }
 }
